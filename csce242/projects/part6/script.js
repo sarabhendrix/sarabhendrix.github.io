0f894c90a6fcc27d07825028cf4796936ffd01b0
Sara Hendrix <seh18@email.sc.edu--global>
2024-10-13 21:28:22 -0400

    project pt 6

--- csce242/projects/part6/script.js
+++ csce242/projects/part6/script.js
@@ -1,6 +1,58 @@
 const toggle = document.getElementById('toggle-menu');
 const menu = document.getElementById('nav-items');
 const arrow = document.getElementById('arrow');
+const getBooks = async() => {
+    const url = "https://sarabhendrix.github.io/csce242/projects/part6/books.json";
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch(error){
+        console.log(error);
+    }
+}
+const showBooks = async() => {
+    const response = await getBooks();
+    const books = response.books;
+
+    books.forEach((book)=>{
+        if(document.getElementById("results")){
+            document.getElementById("results").append(getBookResultSection(book));
+        } else if(document.getElementById("results-larger")){
+            document.getElementById("results-larger").append(getBookResultSectionLarger(book));
+        }
+          
+    });
+};
+
+const getBookResultSection = (book) => {
+    const section = document.createElement("section");
+    section.classList.add("mini-book");
+
+    const image = document.createElement("img");
+    image.src = `images/${book.imagePath}`;
+    section.append(image);
+
+    const name = document.createElement("h5");
+    name.innerHTML = book.title;
+    section.append(name);
+
+    return section;
+}
+
+const getBookResultSectionLarger = (book) => {
+    const section = document.createElement("section");
+    section.id = "small-book";
+
+    const image = document.createElement("img");
+    image.src = `images/${book.imagePath}`;
+    section.append(image);
+
+    const name = document.createElement("h5");
+    name.innerHTML = book.title;
+    section.append(name);
+
+    return section;
+}
 
 toggle.addEventListener('click', () => {
     if (menu.classList.contains('visible')) {
@@ -25,3 +77,4 @@ function resetMenuDisplay() {
 resetMenuDisplay();
 
 window.addEventListener('resize', resetMenuDisplay);
+showBooks();
